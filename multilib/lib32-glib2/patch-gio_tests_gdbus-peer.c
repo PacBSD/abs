d7bcb85c5313bd87f0e954236ba5092210cfbef4
Wolfgang Bumiller <wry.git@bumiller.com>
2013-11-16 20:44:52 +0100

    lib32::glib2: this one's a bit nasty

--- multilib/lib32-glib2/patch-gio_tests_gdbus-peer.c
+++ multilib/lib32-glib2/patch-gio_tests_gdbus-peer.c
@@ -1,11 +1,12 @@
---- gio/tests/gdbus-peer.c.orig	2010-08-16 21:30:03.000000000 +0200
-+++ gio/tests/gdbus-peer.c	2010-08-16 23:13:32.000000000 +0200
-@@ -42,6 +42,8 @@
- #include <sys/socket.h>
+--- gio/tests/gdbus-peer.c	2013-10-19 16:14:25.052047025 +0100
++++ gio/tests/gdbus-peer.c.new	2013-10-19 16:15:45.743041705 +0100
+@@ -44,6 +44,9 @@
+ #include <errno.h>
  #endif
  
 +#include <sys/socket.h>
 +
- /* used in test_overflow */
- #ifdef G_OS_UNIX
- #include <gio/gunixconnection.h>
++
+ #if (defined(__linux__) || \
+   defined(__FreeBSD__) || \
+   defined(__FreeBSD_kernel__) || \
