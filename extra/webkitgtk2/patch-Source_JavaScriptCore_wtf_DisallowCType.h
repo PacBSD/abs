8e0db288e62f673aac7f816b3cc088f03c9d6c2e
Amzo <Amzo@archbsd.net>
2013-12-17 08:50:12 +0000

    upgraded wine / fixed up linuxand flash / new webkitgtk patches

--- extra/webkitgtk2/patch-Source_JavaScriptCore_wtf_DisallowCType.h
+++ extra/webkitgtk2/patch-Source_JavaScriptCore_wtf_DisallowCType.h
@@ -1,6 +1,6 @@
---- Source/WTF/wtf/DisallowCType.h.orig	2013-09-10 12:27:15.000000000 +0200
-+++ Source/WTF/wtf/DisallowCType.h	2013-09-10 12:28:32.000000000 +0200
-@@ -35,7 +35,13 @@
+--- Source/WTF/wtf/DisallowCType.h.orig	2013-12-17 19:10:40.448345616 +0000
++++ Source/WTF/wtf/DisallowCType.h	2013-12-17 19:13:17.203303336 +0000
+@@ -35,11 +35,19 @@
  // ASCII-specific functions instead. This header makes sure we get a compile-time
  // error if we use one of the <ctype.h> functions by accident.
  
@@ -10,7 +10,13 @@
 +#include <cwctype>
 +#include <string>
 +#endif
++
+ // this breaks compilation of <QFontDatabase>, at least, so turn it off for now
+ // Also generates errors on wx on Windows, presumably because these functions
+ // are used from wx headers. On GTK+ for Mac many GTK+ files include <libintl.h>
+ // or <glib/gi18n-lib.h>, which in turn include <xlocale/_ctype.h> which uses
+ // isacii(). 
++
+ #include <wtf/Platform.h>
+ #if !PLATFORM(QT) && !PLATFORM(WX) && !PLATFORM(CHROMIUM) && !(OS(DARWIN) && PLATFORM(GTK)) && !OS(QNX) && !defined(_LIBCPP_VERSION)
  
- #include <ctype.h>
- 
- #undef isalnum
--- extra/webkitgtk2/patch-Tools_DumpRenderTree_gtk_DumpRenderTree.cpp
+++ extra/webkitgtk2/patch-Tools_DumpRenderTree_gtk_DumpRenderTree.cpp
@@ -1,5 +1,5 @@
---- Tools/DumpRenderTree/gtk/DumpRenderTree.cpp.orig	2013-09-10 13:19:16.000000000 +0200
-+++ Tools/DumpRenderTree/gtk/DumpRenderTree.cpp	2013-09-10 13:19:47.000000000 +0200
+--- Tools/DumpRenderTree/gtk/DumpRenderTree.cpp.orig	2012-08-20 18:26:18.000000000 +0200
++++ Tools/DumpRenderTree/gtk/DumpRenderTree.cpp	2013-09-11 12:28:29.000000000 +0200
 @@ -48,6 +48,7 @@
  #include "WorkQueueItem.h"
  #include <JavaScriptCore/JavaScript.h>
@@ -8,14 +8,3 @@
  #include <cstdlib>
  #include <cstring>
  #include <getopt.h>
- 
-patch-Tools_DumpRenderTree_gtk_ImageDiff.cpp:
---- Tools/DumpRenderTree/gtk/ImageDiff.cpp.orig	2013-09-10 13:16:33.000000000 +0200
-+++ Tools/DumpRenderTree/gtk/ImageDiff.cpp	2013-09-10 13:16:39.000000000 +0200
-@@ -30,6 +30,7 @@
- #include <algorithm>
- #include <cmath>
- #include <cstdio>
-+#include <cstdlib>
- #include <cstring>
- #include <gdk/gdk.h>
