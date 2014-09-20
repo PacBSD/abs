3731fc34a97b2a80d94a6cb69f85892466830b61
Amzo <Amzo@archbsd.com>
2014-09-20 15:07:18 +0000

    add some fixes to chromium.sh

--- extra/chromium/chromium.sh
+++ extra/chromium/chromium.sh
@@ -10,6 +10,19 @@ fi
 # default CHROMIUM_FLAGS (from /etc/chromium/default)
 CHROMIUM_FLAGS=${CHROMIUM_USER_FLAGS:-$CHROMIUM_FLAGS}
 
+SYSCTL=kern.ipc.shm_allow_removed
+if [ "`/sbin/sysctl -n $SYSCTL`" = 0 ] ; then
+	cat << EOMSG
+For correct operation, shared memory support has to be enabled
+in Chromium by performing the following command as root :
+
+sysctl $SYSCTL=1
+
+To preserve this setting across reboots, append the following
+to /etc/sysctl.conf :
+
+$SYSCTL=1
+
 export CHROME_WRAPPER=$(readlink -f "$0")
 export CHROME_DESKTOP=chromium.desktop
 
