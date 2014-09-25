feddd2aeecc4203ce408450195825d3fdc7426e1
Wolfgang Bumiller <wry.git@bumiller.com>
2014-09-25 18:03:56 +0200

    chromium: so this works

--- extra/chromium/chromium.sh
+++ extra/chromium/chromium.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/bash
 
+SYSCTL=kern.ipc.shm_allow_removed
+if [ "`/sbin/sysctl -n $SYSCTL`" = 0 ] ; then
+	cat << EOMSG
+For correct operation, shared memory support has to be enabled
+in Chromium by performing the following command as root :
+
+sysctl $SYSCTL=1
+
+To preserve this setting across reboots, append the following
+to /etc/sysctl.conf :
+
+$SYSCTL=1
+EOMSG
+	exit 1
+fi
+
 # Allow users to override command-line options
 # Based on Gentoo's chromium package (and by extension, Debian's)
 if [[ -f /etc/chromium/default ]]; then
