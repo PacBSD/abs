47c17101334d95a508e6b05b63652613f7775432
Amzo <Amzo@archbsd.net>
2015-08-01 16:05:14 +0100

    ffmpeg 2.6.3 -> 2.7.2

--- extra/ffmpeg/files/patch-libavformat-udp.c
+++ extra/ffmpeg/files/patch-libavformat-udp.c
@@ -1,5 +1,5 @@
---- libavformat/udp.c.orig	2015-03-06 23:35:33.000000000 +0100
-+++ libavformat/udp.c	2015-03-14 07:54:42.860295960 +0100
+--- libavformat/udp.c.orig	2015-06-19 20:44:53 UTC
++++ libavformat/udp.c
 @@ -71,6 +71,8 @@
  #define UDP_MAX_PKT_SIZE 65536
  #define UDP_HEADER_SIZE 8
