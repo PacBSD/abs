d1bbe07e7189c43d07d3d3346abf5c41fb9c3dcd
Amzo <Amzo@archbsd.net>
2015-09-23 11:22:59 +0100

    Upgrade xf86-video-intel

--- extra/xf86-video-intel/files/patch-src__sna__sna_accel.c
+++ extra/xf86-video-intel/files/patch-src__sna__sna_accel.c
@@ -1,24 +1,24 @@
---- src/sna/sna_accel.c.orig	2013-11-12 13:13:27.627983913 +0100
-+++ src/sna/sna_accel.c	2013-11-12 13:15:33.756971444 +0100
-@@ -15391,7 +15391,7 @@
+--- src/sna/sna_accel.c.orig	2015-09-23 11:11:40.046461816 +0100
++++ src/sna/sna_accel.c	2015-09-23 11:14:56.126452723 +0100
+@@ -17379,7 +17379,7 @@
  {
  #if HAS_PIXMAP_SHARING
- 	ScreenPtr screen = sna->scrn->pScreen;
+ 	ScreenPtr screen = to_screen_from_sna(sna);
 -	PixmapDirtyUpdatePtr dirty;
 +	PixmapDirtyUpdatePtr dirty = NULL;
  
  	xorg_list_for_each_entry(dirty, &screen->pixmap_dirty_list, ent) {
  		assert(dirty->src == sna->front);
-@@ -15547,7 +15547,7 @@
+@@ -17527,7 +17527,7 @@
  {
  #if HAS_PIXMAP_SHARING
- 	ScreenPtr screen = sna->scrn->pScreen;
+ 	ScreenPtr screen = to_screen_from_sna(sna);
 -	PixmapDirtyUpdatePtr dirty;
 +	PixmapDirtyUpdatePtr dirty = NULL;
- 	bool flush = false;
  
  	xorg_list_for_each_entry(dirty, &screen->pixmap_dirty_list, ent) {
-@@ -15777,7 +15777,8 @@
+ 		RegionRec region, *damage;
+@@ -17790,7 +17790,8 @@
  {
  #if HAS_PIXMAP_SHARING
  	ScreenPtr screen = old_front->drawable.pScreen;
--- extra/xf86-video-intel/files/patch-src__sna__sna_dri2.c
+++ extra/xf86-video-intel/files/patch-src__sna__sna_dri2.c
@@ -1,20 +1,40 @@
---- src/sna/sna_dri2.c.orig	2014-09-03 04:27:11.000000000 -0400
-+++ src/sna/sna_dri2.c	2014-09-16 02:23:52.000000000 -0400
-@@ -162,7 +162,7 @@
+diff --git a/src/sna/sna_dri2.c b/src/sna/sna_dri2.c
+index 13714fb..044e0d3 100644
+--- src/sna/sna_dri2.c.orig
++++ src/sna/sna_dri2.c
+@@ -212,7 +212,7 @@ sna_dri2_cache_bo(struct sna *sna,
+ 		  uint32_t size,
+ 		  uint32_t flags)
+ {
+-	struct dri_bo *c;
++	struct dri_bo *c = NULL;
  
- 	bo = NULL;
- 	if (info) {
+ 	if (draw == NULL) {
+ 		DBG(("%s: no draw, releasing handle=%d\n",
+@@ -265,7 +265,7 @@ sna_dri2_get_back(struct sna *sna,
+ 	struct dri2_window *priv = dri2_window((WindowPtr)draw);
+ 	uint32_t size;
+ 	struct kgem_bo *bo;
+-	struct dri_bo *c;
++	struct dri_bo *c = NULL;
+ 	uint32_t name;
+ 	int flags;
+ 	bool reuse;
+@@ -1509,7 +1509,7 @@ sna_dri2_remove_event(WindowPtr win, struct sna_dri2_event *info)
+ 	priv->chain = info->chain;
+ 	if (priv->chain == NULL) {
+ 		while (!list_is_empty(&priv->cache)) {
+-			struct dri_bo *c;
++			struct dri_bo *c = NULL;
+ 
+ 			c = list_first_entry(&priv->cache, struct dri_bo, link);
+ 			list_del(&c->link);
+@@ -1709,7 +1709,7 @@ void sna_dri2_destroy_window(WindowPtr win)
+ 	}
+ 
+ 	while (!list_is_empty(&priv->cache)) {
 -		struct dri_bo *c;
 +		struct dri_bo *c = NULL;
- 		list_for_each_entry(c, &info->cache, link) {
- 			if (c->bo && c->bo->scanout == 0) {
- 				bo = c->bo;
-@@ -192,7 +192,7 @@
  
- 	if (info && reuse) {
- 		bool found = false;
--		struct dri_bo *c;
-+		struct dri_bo *c = NULL;
- 
- 		list_for_each_entry_reverse(c, &info->cache, link) {
- 			if (c->bo == NULL) {
+ 		c = list_first_entry(&priv->cache, struct dri_bo, link);
+ 		list_del(&c->link);
