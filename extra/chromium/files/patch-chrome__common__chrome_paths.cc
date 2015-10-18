cafd8a8ba6eb88b174604a0d6ecc8a96309d20a5
Amzo <Amzo@archbsd.net>
2015-10-18 16:43:44 +0100

    Upgrade chromium

--- extra/chromium/files/patch-chrome__common__chrome_paths.cc
+++ extra/chromium/files/patch-chrome__common__chrome_paths.cc
@@ -1,6 +1,15 @@
 --- chrome/common/chrome_paths.cc.orig	2014-10-10 08:54:11 UTC
 +++ chrome/common/chrome_paths.cc
-@@ -461,10 +461,12 @@
+@@ -198,7 +198,7 @@
+         return false;
+       break;
+     case chrome::DIR_DEFAULT_DOWNLOADS_SAFE:
+-#if defined(OS_WIN) || defined(OS_LINUX)
++#if defined(OS_WIN) || defined(OS_LINUX) || defined(OS_BSD)
+       if (!GetUserDownloadsDirectorySafe(&cur))
+         return false;
+       break;
+@@ -488,10 +488,12 @@
        if (!base::PathExists(cur))  // We don't want to create this
          return false;
        break;
@@ -10,11 +19,11 @@
  #if defined(GOOGLE_CHROME_BUILD)
        cur = base::FilePath(FILE_PATH_LITERAL("/etc/opt/chrome/policies"));
 +#elif defined(OS_BSD)
-+      cur = base::FilePath(FILE_PATH_LITERAL("/usr/local/etc/chrome/policies"));
++      cur = base::FilePath(FILE_PATH_LITERAL("/etc/chrome/policies"));
  #else
        cur = base::FilePath(FILE_PATH_LITERAL("/etc/chromium/policies"));
  #endif
-@@ -542,7 +544,7 @@
+@@ -557,7 +559,7 @@
  #endif
        break;
  
@@ -23,17 +32,17 @@
      case chrome::DIR_NATIVE_MESSAGING:
  #if defined(OS_MACOSX)
  #if defined(GOOGLE_CHROME_BUILD)
-@@ -556,6 +558,9 @@
+@@ -571,6 +573,9 @@
  #if defined(GOOGLE_CHROME_BUILD)
        cur = base::FilePath(FILE_PATH_LITERAL(
            "/etc/opt/chrome/native-messaging-hosts"));
 +#elif defined(OS_BSD)
 +      cur = base::FilePath(FILE_PATH_LITERAL(
-+          "/usr/local/etc/chrome/native-messaging-hosts"));
++          "/etc/chrome/native-messaging-hosts"));
  #else
        cur = base::FilePath(FILE_PATH_LITERAL(
            "/etc/chromium/native-messaging-hosts"));
-@@ -568,7 +573,7 @@
+@@ -583,7 +588,7 @@
          return false;
        cur = cur.Append(FILE_PATH_LITERAL("NativeMessagingHosts"));
        break;
