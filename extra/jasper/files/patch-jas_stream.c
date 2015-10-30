ce6a56199993cb0b917f0660fbd43fdb3cbb1416
Amzo <Amzo@archbsd.net>
2015-10-30 12:36:44 +0000

    Automatic relbump for jasper

--- extra/jasper/files/patch-jas_stream.c
+++ extra/jasper/files/patch-jas_stream.c
@@ -1,6 +1,25 @@
---- src/libjasper/base/jas_stream.c	Fri Jan 19 16:43:05 2007
-+++ src/libjasper/base/jas_stream.c	Tue Jun 12 10:26:02 2007
-@@ -362,13 +362,12 @@
+--- src/libjasper/base/jas_stream.c.orig	2007-01-19 22:43:05.000000000 +0100
++++ src/libjasper/base/jas_stream.c	2015-08-29 08:07:01.000000000 +0200
+@@ -99,7 +99,7 @@
+ static void jas_stream_destroy(jas_stream_t *stream);
+ static jas_stream_t *jas_stream_create(void);
+ static void jas_stream_initbuf(jas_stream_t *stream, int bufmode, char *buf,
+-  int bufsize);
++  size_t bufsize);
+ 
+ static int mem_read(jas_stream_obj_t *obj, char *buf, int cnt);
+ static int mem_write(jas_stream_obj_t *obj, char *buf, int cnt);
+@@ -168,7 +168,7 @@
+ 	return stream;
+ }
+ 
+-jas_stream_t *jas_stream_memopen(char *buf, int bufsize)
++jas_stream_t *jas_stream_memopen(char *buf, size_t bufsize)
+ {
+ 	jas_stream_t *stream;
+ 	jas_stream_memobj_t *obj;
+@@ -361,15 +361,14 @@
+ 	}
  	obj->fd = -1;
  	obj->flags = 0;
 -	obj->pathname[0] = '\0';
@@ -17,3 +36,40 @@
 +	if (obj->fd < 0) {
  		jas_stream_destroy(stream);
  		return 0;
+ 	}
+@@ -553,7 +552,7 @@
+ 	int ret;
+ 
+ 	va_start(ap, fmt);
+-	ret = vsprintf(buf, fmt, ap);
++	ret = vsnprintf(buf, sizeof(buf), fmt, ap);
+ 	jas_stream_puts(stream, buf);
+ 	va_end(ap);
+ 	return ret;
+@@ -570,7 +569,7 @@
+ 	return 0;
+ }
+ 
+-char *jas_stream_gets(jas_stream_t *stream, char *buf, int bufsize)
++char *jas_stream_gets(jas_stream_t *stream, char *buf, size_t bufsize)
+ {
+ 	int c;
+ 	char *bufptr;
+@@ -694,7 +693,7 @@
+ \******************************************************************************/
+ 
+ static void jas_stream_initbuf(jas_stream_t *stream, int bufmode, char *buf,
+-  int bufsize)
++  size_t bufsize)
+ {
+ 	/* If this function is being called, the buffer should not have been
+ 	  initialized yet. */
+@@ -987,7 +986,7 @@
+ 	return cnt;
+ }
+ 
+-static int mem_resize(jas_stream_memobj_t *m, int bufsize)
++static int mem_resize(jas_stream_memobj_t *m, size_t bufsize)
+ {
+ 	unsigned char *buf;
+ 
