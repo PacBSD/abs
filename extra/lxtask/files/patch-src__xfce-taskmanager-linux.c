0df504aa2e2aae676c4a664411de78b06fe6b5a5
Amzo <amzo@archbsd.com>
2016-06-28 12:27:52 +0000

    drop lxde-gtk3 from lxtask group

--- extra/lxtask/files/patch-src__xfce-taskmanager-linux.c
+++ extra/lxtask/files/patch-src__xfce-taskmanager-linux.c
@@ -1,46 +1,56 @@
---- src/xfce-taskmanager-linux.c.orig	2014-10-19 18:57:15.000000000 +0000
-+++ src/xfce-taskmanager-linux.c	2016-05-03 22:20:11.397891298 +0000
-@@ -41,7 +41,7 @@
- 	task->checked=FALSE;
- 	task->size=0;
+--- ./src/xfce-taskmanager-linux.c.orig	2010-04-12 13:54:32.000000000 +0400
++++ ./src/xfce-taskmanager-linux.c	2010-09-25 23:50:53.956637695 +0400
+@@ -28,6 +28,7 @@
+ #include <glib/gi18n.h>
+ #include "xfce-taskmanager-linux.h"
  
--	sprintf(line,"/proc/%d/statm",(int)pid);
-+	sprintf(line,"/compat/linux/proc/%d/statm",(int)pid);
++#define PAGE_SIZE ( sysconf(_SC_PAGESIZE) )
+ 
+ #if 1
+ void get_task_details(gint pid,struct task *task)
+@@ -39,7 +40,7 @@
+ 	task->checked=FALSE;
+         task->size=0;
+ 
+-	sprintf(line,"/proc/%d/statm",pid);
++	sprintf(line,"/compat/linux/proc/%d/statm",pid);
          fd=open(line,O_RDONLY);
          if(fd==-1) return;
-         ret = read(fd,line,255);
-@@ -57,7 +57,7 @@
- 	task->size = t_size * page_size;
- 	task->rss = t_rss * page_size;
+         read(fd,line,256);
+@@ -49,7 +50,7 @@
+ 	task->size*=PAGE_SIZE;
+ 	task->rss*=PAGE_SIZE;
  
--	sprintf(line,"/proc/%d/stat",(gint)pid);
-+	sprintf(line,"/compat/linux/proc/%d/stat",(gint)pid);
+-	sprintf(line,"/proc/%d/stat",pid);
++	sprintf(line,"/compat/linux/proc/%d/stat",pid);
  	fd=open(line,O_RDONLY);
  	if(fd!=-1)
  	{
-@@ -98,7 +98,7 @@
- 		if(show_full_path)
- 		{
- 			FILE *fp;
--			sprintf(line,"/proc/%d/cmdline",(int)pid);
-+			sprintf(line,"/compat/linux/proc/%d/cmdline",(int)pid);
- 			fp=fopen(line,"r");
- 			if(fp)
- 			{
-@@ -124,7 +124,7 @@
- 		else if(len>=15)
- 		{
- 			FILE *fp;
--			sprintf(line,"/proc/%d/cmdline",(int)pid);
-+			sprintf(line,"/compat/linux/proc/%d/cmdline",(int)pid);
- 			fp=fopen(line,"r");
- 			if(fp)
- 			{
-@@ -192,9 +192,10 @@
-     int n;
+@@ -118,8 +119,8 @@
+     gchar filename[255];
+     gchar cmdline_filename[255];
+ 
+-    sprintf(filename, "/proc/%i/stat", pid);
+-    sprintf(cmdline_filename, "/proc/%i/cmdline", pid);
++    sprintf(filename, "/compat/linux/proc/%i/stat", pid);
++    sprintf(cmdline_filename, "/compat/linux/proc/%i/cmdline", pid);
+ 
+     stat(filename, &status);
+ 
+@@ -235,9 +236,9 @@
+     GArray *task_list;
      int count=0;
  
-+#define PAGE_SIZE ( sysconf(_SC_PAGESIZE) )
+-    if((dir = opendir("/proc/")) == NULL)
++    if((dir = opendir("/compat/linux/proc/")) == NULL)
+     {
+-        fprintf(stderr, "Error: couldn't load the /proc directory\n");
++        fprintf(stderr, "Error: couldn't load the /compat/linux/proc directory\n");
+         return NULL;
+     }
+ 
+@@ -277,7 +278,7 @@
+ 
  
      task_list = g_array_new(FALSE, FALSE, sizeof (struct task));
 -    n=scandir("/proc",&namelist,proc_filter,0);
@@ -48,7 +58,7 @@
      if(n<0) return task_list;
  
      g_array_set_size(task_list,n);
-@@ -216,7 +217,7 @@
+@@ -299,7 +300,7 @@
  
  gboolean get_cpu_usage_from_proc(system_status *sys_stat)
  {
@@ -57,7 +67,7 @@
      FILE *file;
  
      if ( sys_stat->valid_proc_reading == TRUE ) {
-@@ -257,7 +258,7 @@
+@@ -340,7 +341,7 @@
      int reach;
      static int cpu_count;
  
@@ -66,3 +76,12 @@
      if(!file) return FALSE;
      reach=0;
      while (fgets (buffer, 100, file) != NULL)
+@@ -357,7 +358,7 @@
+ 
+     if(!cpu_count)
+     {
+-        file = fopen ("/proc/cpuinfo", "r");
++        file = fopen ("/compat/linux/proc/cpuinfo", "r");
+         if(!file) return FALSE;
+         while (fgets (buffer, 100, file) != NULL)
+         {
