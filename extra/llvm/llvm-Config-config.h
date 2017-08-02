6add0bc8d03bec6277efd9a44b422a0a66ed4e56
Amzo <Amzo@archbsd.com>
2017-08-02 08:48:46 +0000

    Moving these unmaintained PKGBUILDS to somewhere else to keep master tidy

--- extra/llvm/llvm-Config-config.h
+++
@@ -1,9 +0,0 @@
-#include <stdint.h>
-
-#if __WORDSIZE == 32
-#include "config-32.h"
-#elif __WORDSIZE == 64
-#include "config-64.h"
-#else
-#error "Unknown word size"
-#endif
--- extra/xdg-user-dirs/60-xdg-user-dirs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Set up user directories like ~/Destop and ~/Music
-if [ -x /usr/bin/xdg-user-dirs-update ]; then
-  /usr/bin/xdg-user-dirs-update
-fi
